b63cdfd65b893d6008a3521c0addbf0526bf8e5a
Thomas M <thomas.carnegie.1234@gmail.com>
2019-01-28 20:55:18 -0600

    update

--- wsl.sh
+++ wsl.sh
@@ -1,20 +1,3 @@
-# Docker
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get update
-sudo apt-get install docker-ce
 
 # Python 3.6 (Comes installed by default)
 python3 -m pip install --upgrade pip
