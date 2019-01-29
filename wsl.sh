262692a126c3d15053582fd4ae36b11f419bf1ef
Gabriel Britain <quontas@gmail.com>
2019-01-28 20:03:22 -0600

    Wow new WSL script it's crazy wow

---
+++ wsl.sh
@@ -0,0 +1,38 @@
+# Docker
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+
+# Python 3.6 (Comes installed by default)
+python3 -m pip install --upgrade pip
+sudo apt-get install python3-pip
+
+## Python dependencies
+python3 -m pip install flask
+
+# VSCode
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install code
+
+# Git
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install git
