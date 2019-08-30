9a68c7d539716e8e785dc20cfc7c02a2830d8042
Thomas McDonald <thomas.carnegie.1234@gmail.com>
2019-08-30 18:30:00 -0500

    added windows install and added postman to mac

---
+++ install_windows.ps1
@@ -0,0 +1,19 @@
+#install Scoop
+Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+scoop bucket add extras
+
+#install packages
+scoop install python
+scoop install nodejs
+scoop install postman
+scoop install vscode
+scoop install openssh
+[environment]::setenvironmentvariable('GIT_SSH', (resolve-path (scoop which ssh)), 'USER')
+. $PROFILE
+scoop install git
+
+pip3 install flask
+
+
+brew cask install visual-studio-code
